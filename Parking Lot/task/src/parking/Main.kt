8ca60b27a75c56533591e21d41ee79c7827bf2ab
Anatoliy <anartius@gmail.com>
2021-12-16 17:14:10 +0100

    Finish the stage 1.

--- Parking Lot/task/src/parking/Main.kt
+++ Parking Lot/task/src/parking/Main.kt
@@ -1,5 +1,9 @@
 package parking
 
 fun main() {
-    println("Hello, World!")
+    println("""
+        White car has parked.
+        Yellow car left the parking lot.
+        Green car just parked here.
+    """.trimIndent())
 }
